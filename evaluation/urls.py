a7673afd5870ec85dc5a94c2f5e9b9ab6fe31397
Axel García <agarciadarce@gmail.com>
2021-06-10 15:14:05 -0600

    ✨ edicion y eliminacion de notas y evaluaciones de estudiantes

--- evaluation/urls.py
+++ evaluation/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('<int:pk_e>/delete', delete_evaluation, name="deleteevaluation"),
     # recibirá en post los demás datos
     path('<int:pk_e>/puntuation/<int:pk_s>/', create_puntuation, name="createpuntuation"),
-    path('<int:pk_e>/puntuation/<int:pk_s>/update', update_puntuation, name="updatepuntuation"),
+    path('puntuation/<int:pk_p>/update', update_puntuation, name="updatepuntuation"),
+    path('puntuation/<int:pk_p>/delete', delete_puntuation, name="deletepuntuation"),
 ]
--- evaluation/views.py
+++ evaluation/views.py
@@ -17,10 +17,23 @@ def create_evaluation(request, pk_vr):
     
 
 def update_evaluation(request, pk_e):
-    return render(request, "")
+    if request.method == "POST":
+        name = request.POST.get("name")
+        description =  request.POST.get("description")
+        
+        evaluation = Evaluation.objects.get(pk=pk_e)
+        evaluation.name = name
+        evaluation.description = description
+        evaluation.save()
+        messages.info(request, "Evaluación editada correctamente :D")
+    return redirect("virtualroomdetail", pk=evaluation.virtualroom.pk)
 
 def delete_evaluation(request, pk_e):
-    return render(request, "")
+    evaluation = Evaluation.objects.get(pk=pk_e)
+    pk_vr = evaluation.virtualroom.pk
+    evaluation.delete()
+    messages.error(request, "Evaluación eliminada correctamente :D")
+    return redirect("virtualroomdetail", pk=pk_vr)
 
 def create_puntuation(request, pk_e, pk_s):
     if request.method == "POST":
@@ -34,5 +47,21 @@ def create_puntuation(request, pk_e, pk_s):
     
     return redirect("virtualroomdetail", pk=evaluation.virtualroom.pk)
 
-def update_puntuation(request, pk_e, pk_s):
-    return render(request, "")
+def update_puntuation(request, pk_p):
+    if request.method == "POST":
+        puntuation = Puntuation.objects.get(pk=pk_p)
+        amount = request.POST.get("amount")
+        observation = request.POST.get("observation")
+        puntuation.amount = amount
+        puntuation.observation= observation    
+        puntuation.save()
+        messages.warning(request, f"@{puntuation.enrollment.user.username} ha sido calificado (otra vez) ")
+    
+    return redirect("virtualroomdetail", pk=puntuation.evaluation.virtualroom.pk)
+
+def delete_puntuation(request, pk_p):
+    puntuation = Puntuation.objects.get(pk=pk_p)
+    pk_vr = puntuation.evaluation.virtualroom.pk
+    puntuation.delete()
+    messages.error(request, "Puntuación eliminada correctamente :D")
+    return redirect("virtualroomdetail", pk=pk_vr)
