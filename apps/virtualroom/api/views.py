1023c1596b68f872abad204a8462a1f4f86c72de
pacisauctor <agarciadarce@gmail.com>
2021-09-06 15:29:05 -0600

    🎨 add authorization

--- apps/virtualroom/api/views.py
+++ apps/virtualroom/api/views.py
@@ -1,15 +1,15 @@
 from .serializers import VirtualRoomSerializer, EnrollmentSerializer
 from apps.virtualroom.models import VirtualRoom, Enrollment
 from rest_framework import viewsets
-
+from rest_framework.permissions import IsAuthenticated
 
 class VirtualRoomViewSet(viewsets.ModelViewSet):
-
+    permission_classes = (IsAuthenticated,)
     serializer_class = VirtualRoomSerializer
     queryset = VirtualRoom.objects.all()
 
 
 class EnrollmentViewSet(viewsets.ModelViewSet):
-
+    permission_classes = (IsAuthenticated,)
     serializer_class = VirtualRoomSerializer
     queryset = Enrollment.objects.all()
